969eca5c67f2a0e965e08f91444e13c87be22c20
Adrian Hatori <adrianhatori@gmail.com>
2023-04-04 18:19:16 -0300

    a

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfólio | Adrian Hatori</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/layout.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <nav id="header">
+        <div class="container">
+            <div class="navbar navbar-expand-md align-items-between justify-content-between">
+                <a class="navbar-brand" href="https://toridev.com/">
+                    <img src="img/ToriDev.png" alt="" srcset="" style="max-width:40px; max-height: 40px;">
+                    <strong id="brand-text-1">Tori</strong><strong id="brand-text-2">Dev</strong>
+                </a>
+                <div class="contact-box">
+                    <a href=""><i class="fa fa-brands fa-linkedin px-1"></i></a>
+                    <a href=""><i class="fa fa-brands fa-github px-1"></i></a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <section id="sectionBackground">
+        <div class="container">
+            <div id="banner">
+                <div class="row gx-2 align-items-center">
+                    <div id="adrian-image" class="col-md-6">
+                        <img class="col-12 rounded-5" alt="Adrian_Hatori" src="img/Adrian_Hatori_Sem_Fundo.png">
+                    </div>
+                    <div id="adrian-desc" class="col-md-6">
+                        <div class="text-center">
+                            <p>
+                            <h2><strong id="adrian-name">ADRIAN LEMES HATORI</strong></h2>
+                            </p>
+                            <p id="adrian-course">Desenvolvedor Web</p>
+                            <p id="adrian-course">Análise e desenvolvimento de sistemas</p>
+                        </div>
+                        <div class="col-12 d-flex flex-row justify-content-around my-3">
+                            <img align="center" alt="Hatori-HTML5" class="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"/>
+                            <img align="center" alt="Hatori-CSS3" class="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"/>
+                            <img align="center" alt="Hatori-BS5" class="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"/>
+                            <img align="center" alt="Hatori-JS" class="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"/>
+                            <img align="center" alt="Hatori-PHP" class="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg"/>
+                            <img align="center" alt="Hatori-MySQL" class="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"/>
+                            <img align="center" alt="Hatori-Git" class="col-1" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"/>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div id="projects" class="mt-5 mb-3">
+                <div class="column text-center">
+                    <h3>PROJETOS</h3>
+                    <div class="text-center">
+                        <div class="" id="projeto-1">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-2">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-3">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-1">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-2">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-3">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-1">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-2">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-3">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-1">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-2">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-3">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-1">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-2">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-3">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-1">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-2">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-3">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-1">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-2">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-3">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-1">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-2">
+                            https://codeportal.epizy.com/
+                        </div>
+                        <div class="" id="projeto-3">
+                            https://codeportal.epizy.com/
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <footer class="text-center p-2" id="copyright">
+        © 2023 Copyright:
+        <a href=""><strong id="copysite">ToriDev.com</strong></a>
+    </footer>
+
+    <script src="js/index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/b217452130.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
